5033010e5d992b746ac909399b29349969b0f3b3
Dynin Mikhail <dynin.michael21@gmail.com>
2020-06-10 16:42:56 +0300

    deleted non-used things

--- js/main.js
+++ js/main.js
@@ -26,21 +26,3 @@ $(document).on('click' , '#gr_find', function(){
     });
     return false;
 });
-
-
-$("[data-tooltip]").mousemove(function (eventObject) {
-		$data_tooltip = $(this).attr("data-tooltip");
-		$("#tooltip").text($data_tooltip)
-								 .css({
-										 "top" : eventObject.pageY + 5,
-										"left" : eventObject.pageX + 5
-								 })
-								 .show();
-}).mouseout(function () {
-		$("#tooltip").hide()
-								 .text("")
-								 .css({
-										 "top" : 0,
-										"left" : 0
-								 });
-});
